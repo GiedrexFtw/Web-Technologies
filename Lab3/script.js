ea26e28693d7ea023ec3ee96eb6b3b7f3a430afc
Giedrius <gb7787@student.uni-lj.si>
2020-03-22 23:55:59 +0100

    lab3 with some bugs

--- Lab3/script.js
+++ Lab3/script.js
@@ -1,31 +1,46 @@
 "use strict";
-
 function domRemoveParticipant(event) {
-    // TODO
+    console.log($(this).text());
 }
 
 function domAddParticipant(participant) {
-    // TODO
+    $("#participant-table")
+    .append(
+    `<tr>
+    <td>${participant.first}</td>
+    <td>${participant.last}</td>
+    <td>${participant.role}</td>
+    </tr>`
+    );
 }
 
-function addParticipant(event) {
+function addParticipant(id, participants) {
     // TODO: Get values
-    const first = "";
-    const last = "";
-    const role = "";
-    
+    const first = $("#first").val();
+    const last = $("#last").val();
+    const role = $("#role").val();
+    //Checking if I get correct values
+    console.log(`${first} ${last} ${role} ${id+1}`);
     // TODO: Set input fields to empty values
     // ...
+    $("#first").val("");
+    $("#last").val("");
+    $("#role").val("");
     
     // Create participant object
     const participant = {
+        id:++id,
         first: first,
         last: last,
         role: role
     };
+    participants.push(participant);
+    console.log(participants);
 
     // Add participant to the HTML
     domAddParticipant(participant);
+    let participantsString=JSON.stringify(participants);
+    localStorage.setItem("participants", participantsString);
 
     // Move cursor to the first name input field
     document.getElementById("first").focus();
@@ -34,10 +49,31 @@ function addParticipant(event) {
 document.addEventListener("DOMContentLoaded", () => {
     // This function is run after the page contents have been loaded
     // Put your initialization code here
-    document.getElementById("addButton").onclick = addParticipant;
+    let id;
+    let participants;
+    if(localStorage.getItem("participants")==null){
+        id=0;
+        participants=new Array();
+    }
+    else{
+        
+        participants=JSON.parse(localStorage.getItem("participants"));
+        id=participants.length;
+    }
+    $("#addButton").click(function(){
+        addParticipant(id, participants); 
+    });
+    //$("tr").dblclick(domRemoveParticipant);
+    
+
 })
 
 // The jQuery way of doing it
 $(document).ready(() => {
     // Alternatively, you can use jQuery to achieve the same result
+    //Yep: 
+    /*$("#addButton").click(function(){
+        addParticipant;
+    })
+    */
 });
